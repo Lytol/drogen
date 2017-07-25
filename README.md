7e12124d8855e1b756363fa6ce9c4a55c240c3ea
Brian Smith <brian.e.smith@gmail.com>
2017-07-24 23:27:00 -0700

    Update README and remove default CSS positioning

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A Drogen is a pulsating marker used for drawing attention to specific page eleme
 Usage
 -----
 
-    <Drogen size=32 color=#cc3333 top=50% left=0>
+    <Drogen size="32px" color="#03c" top="50%" left={0}>
       <h1>My Header</h1>
     </Drogen>
 
@@ -16,5 +16,14 @@ Usage
 
 - `size`: The size of the Drogen  (default: `32px`)
 - `color`: The color of the Drogen (default: `#39c`)
-- `top`, `right`, `bottom`, `left`: The position of the Drogen relative the children (default top: 50%, left: 0)
-- `onClick`: The action to perform if the Drogen is clicked
+- `top` / `left`: The position of the Drogen relative the children (default `top: 50%`, `left: 0`)
+
+
+TODO
+----
+
+* Better positioning than top and left (duh)
+* OnClick handling for custom functionality
+* Optional title text on hover
+* Eslint for style consistency
+* Tests!
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types'
 import styles from './index.css'
 
 const Dot = (props) => {
+  const position = {
+    top: props.top,
+    left: props.left
+  }
   const override = {
     width: props.size,
     height: props.size,
@@ -10,7 +14,7 @@ const Dot = (props) => {
   }
 
   return (
-    <div className={styles.dot}>
+    <div className={styles.dot} style={position}>
       <div className={styles.outer} style={override} />
       <div className={styles.inner} style={override} />
     </div>
@@ -19,12 +23,12 @@ const Dot = (props) => {
 
 class Drogen extends React.Component {
   render() {
-    const {children, color, size} = this.props
+    const {children, color, size, top, left} = this.props
 
     return (
       <div className={styles.drogen}>
         {children}
-        <Dot color={color} size={size} />
+        <Dot color={color} size={size} top={top} left={left} />
       </div>
     )
   }
@@ -33,12 +37,16 @@ class Drogen extends React.Component {
 Drogen.propTypes = {
   children: PropTypes.element.isRequired,
   color: PropTypes.string.isRequired,
-  size: PropTypes.string.isRequired
+  size: PropTypes.string.isRequired,
+  top: PropTypes.oneOfType([ PropTypes.string, PropTypes.number ]),
+  left: PropTypes.oneOfType([ PropTypes.string, PropTypes.number ])
 }
 
 Drogen.defaultProps = {
   color: "#39c",
-  size: "32px"
+  size: "32px",
+  top: 0,
+  left: 0
 }
 
 export default Drogen
